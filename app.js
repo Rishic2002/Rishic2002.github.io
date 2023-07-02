dd6998a442f6fd3b35a83c1ba96d9e89420a4e78
Rishi Chhabra <73890141+Rishic2002@users.noreply.github.com>
2023-07-02 10:03:50 +0530

    Version 1.0 , links pending

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 (function () {
+    const themeToggleBtn = document.querySelector(".theme-btn");
+  
+    // Function to detect system theme preference
+    function detectSystemTheme() {
+      if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        document.body.classList.add("dark-mode");
+        document.body.classList.remove("light-mode");
+      } else {
+        document.body.classList.remove("dark-mode");
+        document.body.classList.add("light-mode");
+      }
+    }
+  
+    // Function to toggle between light and dark mode
+    function toggleTheme() {
+      document.body.classList.toggle("dark-mode");
+      document.body.classList.toggle("light-mode");
+    }
+  
+    // Event listener for theme toggle button
+    themeToggleBtn.addEventListener("click", toggleTheme);
+  
+    // Call the function to detect system theme preference on page load
+    detectSystemTheme();
+  
+    // Event listeners for individual control buttons
     [...document.querySelectorAll(".control")].forEach(button => {
-        button.addEventListener("click", function() {
-            document.querySelector(".active-btn").classList.remove("active-btn");
-            this.classList.add("active-btn");
-            document.querySelector(".active").classList.remove("active");
-            document.getElementById(button.dataset.id).classList.add("active");
-        })
+      button.addEventListener("click", function () {
+        document.querySelector(".active-btn").classList.remove("active-btn");
+        this.classList.add("active-btn");
+        document.querySelector(".active").classList.remove("active");
+        document.getElementById(button.dataset.id).classList.add("active");
+      });
     });
-    document.querySelector(".theme-btn").addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
-    })
-})();
+  
+    // Event listener for system theme change
+    if (window.matchMedia) {
+      window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", detectSystemTheme);
+    }
+  })();
+  
